1ca258ec350a2b4014f44bf258d3028b1049208d
raul782 <181557+raul782@users.noreply.github.com>
2021-04-12 08:07:24 -0500

    fix: Replace CI token with Personal token

--- packages/bessel-uikit/src/index.ts
+++ packages/bessel-uikit/src/index.ts
@@ -37,6 +37,6 @@ export * from "./widgets/Menu";
 export * from "./widgets/Toast";
 export * from "./widgets/WalletModal";
 
-// Base Theme - default as reset
+// Base Theme - default as ResetCSS
 export { default as ResetCSS } from "./ResetCSS";
 export * from "./theme";
